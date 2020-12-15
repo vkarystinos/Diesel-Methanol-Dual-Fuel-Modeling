fa61467756ebbd2065f7c16e7e43bf491dafdd00
vkarystinos <73643522+vkarystinos@users.noreply.github.com>
2020-12-15 11:21:51 +0200

    Add files via upload

---
+++ wiebe_plots.jl
@@ -0,0 +1,24 @@
+### A Pluto.jl notebook ###
+# v0.12.17
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 7a9502b0-3eb2-11eb-3793-d71407022bb5
+md"# Wiebe Function Plots
+
+_Created by V. Karystinos_
+
+This notebook plots the single and double wiebe function, using the predifined coeeficients from the user. Also additional data from the user can be provided in order to compare the the shape of wiebe function with the actual heat realese fraction.
+"
+
+# ╔═╡ 14bd33dc-3eb4-11eb-2ccc-d1b462dfe746
+md" ## _Wiebe Function_
+
+Wiebe function purpose is to model 
+
+"
+
+# ╔═╡ Cell order:
+# ╟─7a9502b0-3eb2-11eb-3793-d71407022bb5
+# ╟─14bd33dc-3eb4-11eb-2ccc-d1b462dfe746
